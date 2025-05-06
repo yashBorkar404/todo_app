7bafe54632d1e6b9eacc81c1e66ae2ae5bee1341
Suchit G <suchitg04@gmail.com>
2025-05-06 11:48:38 +0530

    bruh

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',  // Enables static exports
-  distDir: 'out',    // Output directory for the static build
-  images: {
-    unoptimized: true, // For static export, images need to be unoptimized
-  },
-  // Required for GitHub Pages deployment with repository name in the URL path
-  basePath: process.env.NODE_ENV === 'production' ? '/devopslab_todo_app' : '',
+  basePath: '/devopslab_todo_app',
 };
 
 export default nextConfig;
